7ae96fc40a51a27d8a066392edd7c2677966b6fb
Ed-crze <144076508+Ed-crze@users.noreply.github.com>
2026-05-03 01:04:37 +0000

    Replace firebase config import with inline config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
+export const firebaseConfig = {
+  projectId: "graceful-system-473712-a1",
+  appId: "1:556744244171:web:9c0f41fa5348eaad01544e",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "graceful-system-473712-a1.firebaseapp.com",
+  firestoreDatabaseId: "ai-studio-49a9a9e1-a14f-47cd-9b51-8742bcc80997",
+  storageBucket: "graceful-system-473712-a1.firebasestorage.app",
+  messagingSenderId: "556744244171",
+  measurementId: ""
+}
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
